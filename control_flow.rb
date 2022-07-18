eecd878ece5c2b4d6668e6726018ba1ec0fe88ae
Nathaniel Davydov <nathanielmdavydov@gmail.com>
2022-07-18 15:49:26 -0400

    commit tree 33539fc0ed6d038e7fd70700e76fae151d045b89

--- control_flow.rb
+++ control_flow.rb
@@ -1,9 +1,23 @@
 def admin_login(username, password)
   # your code here
+  if username == "admin" && password == "12345" || username == "ADMIN" && password == "12345"
+    return "Access granted"
+  else 
+    return "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
   # your code here
+  if temperature < 40 
+    return "It's brisk out there!"
+  elsif temperature > 40 && temperature < 65
+    return "It's a little chilly out there"
+  elsif temperature > 85 
+    return "It's too dang hot"
+  else 
+    return "It's perfect out here"
+  end
 end
 
 def fizzbuzz(num)
